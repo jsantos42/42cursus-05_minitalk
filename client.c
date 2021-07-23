20d300a1f34d893392a74808896c6b44a23ef775
Joao Santos <jsantos@c2r1s5.42lisboa.com>
2021-07-23 18:05:41 +0100

    testing pointers to function

--- client.c
+++ client.c
@@ -1,12 +1,27 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+//delete the following 3
+#include <stdio.h>  //this can go
+#include <stdlib.h>  //this can go
+#include <string.h>
 
-int main()
+void function(char *str)
 {
-	while (1)
-	{
-		printf("Hello process number %d\n", getpid());
-		sleep(1);
-	}
+	printf("%s", str);
+}
+
+int main(int argc, char **argv)
+{
+	int		pid;
+	char	*str;
+
+	if (argc != 3)
+		return (1);
+	pid = atoi(argv[1]); // replace with my atoi function
+	str = strcpy(str, argv[2]); //replace with my strcpy function
+//	signal(pid, function(str));
+//	kill(pid, 9);
+
+
     return 0;
 }
--- server.c
+++ server.c
@@ -1,12 +1,50 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+
+void	handler(int sig_num);
+
+void choose_function(void(*func_name)(int), int number);
+void print_hello(int num);
+void print_goodbye(int num);
 
 int main()
 {
-	while (1)
-	{
-		printf("Hello process number %d\n", getpid());
-		sleep(1);
-	}
+	int iter;
+	int signal;
+
+	choose_function(print_hello, 29);
+	choose_function(print_goodbye, 29);
+//	signal(SIGUSR1, &handler);
+
+
+//	printf("Hello process number %d\n", getpid());
+
+//	while (1)
+//	{
+//		//if gets string, print it
+//		sleep(1);
+//	}
     return 0;
+}
+
+void	handler(int sig_num)
+{
+
+}
+
+
+void print_hello(int num)
+{
+	printf("hello, I'm number %d\n", num);
+}
+
+void print_goodbye(int num)
+{
+	printf("goodbye, I'm number %d\n", num);
+}
+
+void choose_function(void(*func_name)(int), int number)
+{
+	func_name(number);
 }
